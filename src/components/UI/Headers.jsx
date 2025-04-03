411b9755346459ab964b3d0b62c023b868e99870
Rushikesh Padaval <rushikeshpadaval@outlook.com>
2025-04-03 18:10:24 +0530

    first commit"

---
+++ src/components/UI/Headers.jsx
@@ -0,0 +1,17 @@
+export const Headers = ()=>{
+ return(
+    <Header>
+        <div className="container">
+            <div className="grid navbar-grid">
+                <div className="Logo">
+
+                <h1>WorldAtlas</h1>
+                </div>
+
+
+            </div>
+        </div>
+
+    </Header>
+ )
+};
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,24 @@
+import { NavLink, useRouteError } from "react-router-dom";
+
+
+export const ErrorPage =()=>{
+    const error = useRouteError();
+    console.log(error);
+    
+    return (
+        <div>
+            <h1>
+                oops! An error occurred
+            </h1>
+            {error && <p>{error.data}</p>}
+            <NavLink to={'/'} >
+            <button>
+            Go Home
+            </button>
+            </NavLink>
+        </div>
+    )
+
+
+    return <h1>Error Page</h1>
+};
