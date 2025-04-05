be0393a27579e6415b412728ab7542ea1427272f
Rushikesh Padaval <rushikeshpadaval@outlook.com>
2025-04-05 19:10:40 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,26 @@
+import { FaArrowRight } from "react-icons/fa";
+
 export const Home = () =>{
-    return<h1>Home Page</h1>
+return (
+    <main className="hero-section main" >
+        <div className="container grid grid-two-cols" >
+<div className="hero-content" >
+    <h1 className="heading-xl">
+        Explore the world One Country at a time .
+
+    </h1>
+    <p className="paragraph" >
+        Discover the history, culture , and beauty of every nation. Sort, Search, and filter through countries to find the details .
+    </p>
+    <button className="btn btn-darken btn-inline bg-white-box " >
+        Start Exploring<FaArrowRight /> 
+
+    </button>
+</div>
+<div className="hero-image" >
+    <img src="/images/nasa-Q1p7bh3SHj8-unsplash.jpg" alt="world beauty" className="banner-image" />
+</div>
+        </div>
+    </main>
+)
 };
