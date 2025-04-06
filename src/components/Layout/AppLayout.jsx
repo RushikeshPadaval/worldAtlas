7f1442e98454efbec615fe4ac08d1d94b74d159e
rushi padaval <rushikeshpadaval@outlook.com>
2025-04-06 11:15:23 +0530

    changes

--- src/components/Layout/AppLayout.jsx
+++ src/components/Layout/AppLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
-import { Footers } from "../UI/Footer"
 import { Headers } from "../UI/Headers"
+import { Footers } from "../UI/Footer"
 
 export  const AppLayout = () =>{
 return(
